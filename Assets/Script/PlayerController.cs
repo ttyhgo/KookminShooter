91f8aa2c5a75b441dab30796b0fb9e962c9f4a3c
ttyhgo <ttyhgo@gmail.com>
2016-10-04 15:59:13 +0900

    First Study

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody rb;
+	public float speed;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rb.velocity = new Vector3 (moveHorizontal, 0, moveVertical) * speed;
+	}
+}
